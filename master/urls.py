ecdf55fa6224679175696b802a5b22c4093ba884
elyas <elyashedayat1380@gmail.com>
2022-04-11 03:36:26 +0430

    refactor master urls

--- master/urls.py
+++ master/urls.py
@@ -6,9 +6,9 @@ from .views import (MasterCreateView, MasterDeleteView, MasterDetailView,
 app_name = "Master"
 
 urlpatterns = [
-    path('', MasterListView.as_view(), name='master_list'),
-    path('<int:id>/', MasterDetailView.as_view(), name='master_detail'),
-    path('update/<int:id>/', MasterUpdateView.as_view(), name='master_update'),
-    path('delete/<int:id>/', MasterDeleteView.as_view(), name='master_delete'),
-    path('create/', MasterCreateView.as_view(), name='master_create'),
+    path('', MasterListView.as_view(), name='List'),
+    path('<int:id>/', MasterDetailView.as_view(), name='Detail'),
+    path('update/<int:id>/', MasterUpdateView.as_view(), name='Update'),
+    path('delete/<int:id>/', MasterDeleteView.as_view(), name='Delete'),
+    path('create/', MasterCreateView.as_view(), name='Create'),
 ]
