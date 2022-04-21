f98a8a18abd318d0bda07d9aa9f802e57b844e45
elyas <elyashedayat1380@gmail.com>
2022-04-21 18:23:21 +0430

    overwrite dispatch method in sitesetting create view

--- config/views.py
+++ config/views.py
@@ -5,9 +5,10 @@ from django.views.generic import (
     UpdateView,
 
 )
+from django.http import Http404
 from django.urls import reverse_lazy
 from .forms import SiteSettingForm
-from .models import SiteSetting
+from .models import SiteSetting, IPAddress
 from django.contrib import messages
 from extenstion.mixins import SuperuserMixin
 
@@ -20,34 +21,39 @@ class PanelView(TemplateView):
 
 
 class SiteSettingCreateView(SuperuserMixin, CreateView):
-    template_name = ''
+    template_name = 'config/create.html'
     form_class = SiteSettingForm
     model = SiteSetting
     success_url = reverse_lazy()
 
+    def dispatch(self, request, *args, **kwargs):
+        ip_address = self.request.user.ip_address
+        if IPAddress.objects.all().count() < 1:
+            IPAddress.objects.create(ip_address=ip_address)
+            return super(SiteSettingCreateView, self).dispatch(request, *args, **kwargs)
+        raise Http404
+
     def form_valid(self, form):
-        messages.success()
+        messages.success(self.request, '', '')
         return super(SiteSettingCreateView, self).form_valid(form)
 
     def form_invalid(self, form):
-        messages.error()
+        messages.error(self.request, '', '')
         return super(SiteSettingCreateView, self).form_invalid(form)
 
 
 class SiteSettingUpdateView(SuperuserMixin, UpdateView):
-    template_name = ''
+    template_name = 'config/update.html'
     form_class = SiteSettingForm
     model = SiteSetting
-    success_url = reverse_lazy()
-    slug_field = ''
-    slug_url_kwarg = ''
+    success_url = reverse_lazy("config:Panel")
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
 
     def form_valid(self, form):
-        messages.success()
+        messages.success(self.request, '', '')
         return super(SiteSettingUpdateView, self).form_valid(form)
 
     def form_invalid(self, form):
-        messages.error()
+        messages.error(self.request, '', '')
         return super(SiteSettingUpdateView, self).form_invalid(form)
-
-
