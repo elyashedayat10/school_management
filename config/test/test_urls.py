80a7f00fff7ad8f9241b54ceaa48c1759a6d1d55
elyas <elyashedayat1380@gmail.com>
2022-04-25 07:15:47 +0430

    writing tests for config urls

---
+++ config/test/test_urls.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from ..views import (
+    PanelView,
+    SiteSettingCreateView,
+    SiteSettingUpdateView,
+
+)
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_panel_view(self):
+        url = reverse('config:Panel')
+        self.assertEqual(resolve(url).func.view_class, PanelView)
+
+    def test_site_setting_create_view(self):
+        url = reverse('config:create')
+        self.assertEqual(resolve(url).func.view_class, SiteSettingCreateView)
+
+    def test_update_setting_update_view(self):
+        url = reverse('config:update', args=(1,))
+        self.assertEqual(reverse(url).func.view_class, SiteSettingUpdateView)
